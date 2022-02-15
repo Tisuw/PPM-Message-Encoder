2f451d060ad3bc17009cee3613626205cc864451
Gregor Campbell <gwc2000@hw.ac.uk>
2022-02-15 16:48:33 +0000

    getPPM v1 implemented

--- steg.c
+++ steg.c
@@ -5,117 +5,141 @@
 
 /* The RGB values of a pixel. */
 struct Pixel {
-    int red;
-    int green;
-    int blue;
+	int red;
+	int green;
+	int blue;
 };
 
-/* An image loaded from a PPM file. */
+/* An image	loaded from	a PPM file.	*/
 struct PPM {
-    /* TODO: Question 1 */
+	/*	TODO: Question 1 */
+	int height;
+	int width;
+	int max;
+	struct Pixel *data;
 };
 
-/* Reads an image from an open PPM file.
- * Returns a new struct PPM, or NULL if the image cannot be read. */
-struct PPM *getPPM(FILE * f)
+/* Reads an	image from an open PPM file.
+ * Returns a new struct	PPM, or	NULL if	the	image cannot be	read. */
+struct PPM *getPPM(FILE	* f)
 {
-    /* TODO: Question 2a */
-    return NULL;
+	/*	TODO: Question 2a */
+	FILE *fin = fopen(argv[1], "r");
+	
+	if (fin == NULL){
+		printf("steg: can't open %s\n", argv[1]);
+		return NULL
+	}
+	
+	struct PPM ppm1;
+	
+	fscanf(fin, "%*s %d", ppm1.width);
+	fscanf(fin, "%d", ppm1.height);
+	fscanf(fin, "%d", ppm1.max);
+	
+	ppm1.data = calloc((ppm1.width*ppm1.height) * sizeof(Pixel));
+	
+	for (int i = 0; i < (ppm1.width*ppm1.height); i++)){
+		fscanf(fin, "%d %d %d", ppm1.data[i].red, ppm1.data[i].green, ppm1.data[i].blue);
+	}
+		
+		
+	return ppm1;
 }
 
-/* Write img to stdout in PPM format. */
+/* Write img to	stdout in PPM format. */
 void showPPM(const struct PPM *img)
 {
-    /* TODO: Question 2b */
+	/*	TODO: Question 2b */
 }
 
-/* Opens and reads a PPM file, returning a pointer to a new struct PPM.
- * On error, prints an error message and returns NULL. */
+/* Opens and reads a PPM file, returning a pointer to a	new	struct PPM.
+ * On error, prints	an error message and returns NULL. */
 struct PPM *readPPM(const char *filename)
 {
-    /* Open the file for reading */
-    FILE *f = fopen(filename, "r");
-    if (f == NULL) {
-        fprintf(stderr, "File %s could not be opened.\n", filename);
-        return NULL;
-    }
+	/*	Open the file for reading */
+	FILE *f = fopen(filename, "r");
+	if	(f == NULL)	{
+		fprintf(stderr, "File	%s could not be	opened.\n",	filename);
+		return NULL;
+	}
 
-    /* Load the image using getPPM */
-    struct PPM *img = getPPM(f);
+	/*	Load the image using getPPM	*/
+	struct	PPM	*img = getPPM(f);
 
-    /* Close the file */
-    fclose(f);
+	/*	Close the file */
+	fclose(f);
 
-    if (img == NULL) {
-        fprintf(stderr, "File %s could not be read.\n", filename);
-        return NULL;
-    }
+	if	(img ==	NULL) {
+		fprintf(stderr, "File	%s could not be	read.\n", filename);
+		return NULL;
+	}
 
-    return img;
+	return	img;
 }
 
-/* Encode the string text into the red channel of image img.
- * Returns a new struct PPM, or NULL on error. */
+/* Encode the string text into the red channel of image	img.
+ * Returns a new struct	PPM, or	NULL on	error. */
 struct PPM *encode(const char *text, const struct PPM *img)
 {
-    /* TODO: Question 2c */
-    return NULL;
+	/*	TODO: Question 2c */
+	return	NULL;
 }
 
-/* Extract the string encoded in the red channel of newimg, by comparing it
- * with oldimg. The two images must have the same size.
+/* Extract the string encoded in the red channel of	newimg,	by comparing it
+ * with	oldimg.	The	two	images must	have the same size.
  * Returns a new C string, or NULL on error. */
-char *decode(const struct PPM *oldimg, const struct PPM *newimg)
+char *decode(const struct PPM *oldimg, const struct	PPM	*newimg)
 {
-    /* TODO: Question 2d */
-    return NULL;
+	/*	TODO: Question 2d */
+	return	NULL;
 }
 
-/* TODO: Question 3 */
-int main(int argc, char *argv[])
+/* TODO: Question 3	*/
+int	main(int argc, char	*argv[])
 {
-    /* Initialise the random number generator, using the time as the seed */
-    srand(time(NULL));
+	/*	Initialise the random number generator,	using the time as the seed */
+	srand(time(NULL));
 
-    /* Parse command-line arguments */
-    if (argc == 3 && strcmp(argv[1], "t") == 0) {
-        /* Mode "t" - test PPM reading and writing */
+	/*	Parse command-line arguments */
+	if	(argc == 3 && strcmp(argv[1], "t") == 0) {
+		/* Mode "t" -	test PPM reading and writing */
 
-        struct PPM *img = readPPM(argv[2]);
-        showPPM(img);
+		struct PPM *img =	readPPM(argv[2]);
+		showPPM(img);
 
-    } else if (argc == 3 && strcmp(argv[1], "e") == 0) {
-        /* Mode "e" - encode PPM */
+	} else	if (argc ==	3 && strcmp(argv[1], "e") == 0)	{
+		/* Mode "e" -	encode PPM */
 
-        struct PPM *oldimg = readPPM(argv[2]);
+		struct PPM *oldimg = readPPM(argv[2]);
 
-        /* TODO: prompt for a message from the user, and read it into a string */
+		/* TODO: prompt for a	message	from the user, and read	it into	a string */
 
-        struct PPM *newimg;
-        /* TODO: encode the text into the image with encode, and assign to newimg */
+		struct PPM *newimg;
+		/* TODO: encode the text into	the	image with encode, and assign to newimg	*/
 
-        /* TODO: write the image to stdout with showPPM */
+		/* TODO: write the image to stdout with showPPM */
 
-    } else if (argc == 4 && strcmp(argv[1], "d") == 0) {
-        /* Mode "d" - decode PPM */
+	} else	if (argc ==	4 && strcmp(argv[1], "d") == 0)	{
+		/* Mode "d" -	decode PPM */
 
-        struct PPM *oldimg;
-        /* TODO: get original file filename from argv, load it with
-           readPPM, then assign to oldimg */
+		struct PPM *oldimg;
+		/* TODO: get original	file filename from argv, load it with
+			 readPPM, then assign to	oldimg	*/
 
-        struct PPM *newimg;
-        /* TODO: get encoded file filename from argv, load it with
-           readPPM, then assign to newimg */
+		struct PPM *newimg;
+		/* TODO: get encoded file	filename from argv,	load it	with
+			 readPPM, then assign to	newimg	*/
 
-        char *message;
-        /* TODO: decode the encodedPPM with the comparisonPPM and assign to message */
+		char *message;
+		/* TODO: decode the encodedPPM with the comparisonPPM	and	assign to message */
 
-        /* TODO: print the decoded message to stdout */
+		/* TODO: print the decoded message to	stdout */
 
-    } else {
-        fprintf(stderr, "Unrecognised or incomplete command line.\n");
-        return 1;
-    }
+	} else	{
+		fprintf(stderr, "Unrecognised	or incomplete command line.\n");
+		return 1;
+	}
 
-    return 0;
+	return	0;
 }
