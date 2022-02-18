2bf63bf8d4c2349978d60e16525a4325d725f49b
Gregor Campbell <gwc2000@hw.ac.uk>
2022-02-18 15:52:14 +0000

    Added randomiser to encoder and solved (same value as character) red pixels mostly

--- steg.c
+++ steg.c
@@ -16,6 +16,7 @@ struct PPM {
 	int height;
 	int width;
 	int max;
+	int numPix;
 	struct Pixel *data;
 };
 
@@ -31,9 +32,11 @@ struct PPM *getPPM(FILE	* f)
 	fscanf(f, "%i", &(ppm1->height));
 	fscanf(f, "%i", &(ppm1->max));
 	
-	ppm1->data = calloc(((ppm1->width)*(ppm1->height)), sizeof(struct Pixel));
+	ppm1->numPix = (ppm1->width)*(ppm1->height);
 	
-	for (int i = 0; i < ((ppm1->width)*(ppm1->height)); i++){
+	ppm1->data = calloc(ppm1->numPix, sizeof(struct Pixel));
+	
+	for (int i = 0; i < (ppm1->numPix); i++){
 		fscanf(f, "%i %i %i", &((ppm1->data)[i].red), &((ppm1->data)[i].green), &((ppm1->data)[i].blue));
 	}
 	
@@ -46,7 +49,7 @@ struct PPM *getPPM(FILE	* f)
 void showPPM(const struct PPM *img)
 {
 	/*	TODO: Question 2b */
-	for(int i = 0; i<(img->height*img->width); i++){
+	for(int i = 0; i<(img->numPix); i++){
 		if(i%img->width == 0){
 			printf("\n");
 		}
@@ -86,8 +89,9 @@ struct PPM *dcopy(const struct PPM *img){
 	newimg->height = img->height;
 	newimg->width = img->width;
 	newimg->max = img->max;
-	newimg->data = calloc(((newimg->width)*(newimg->height)), sizeof(struct Pixel));
-	for (int i = 0; i < ((newimg->width)*(newimg->height)); i++){
+	newimg->numPix = img->height*newimg->width;
+	newimg->data = calloc((newimg->numPix), sizeof(struct Pixel));
+	for (int i = 0; i < (newimg->numPix); i++){
 		newimg->data[i].red = img->data[i].red;
 		newimg->data[i].green = img->data[i].green;
 		newimg->data[i].blue = img->data[i].blue;
@@ -108,19 +112,36 @@ struct PPM *encode(const char *text, const struct PPM *img)
 	int len = strlen(text); // readability
 	
 	//check the image is big enough for the message
-	if (len > ((newimg->width)*(newimg->height))){
+	if (5*(len+1) > (newimg->numPix)){
 		printf("Error: message too big for file");
 		exit(1);
 	}
-	int start = (rand() % len);
+	int start;
+	do {
+		//Pick a start point for the message
+		start = (rand() % newimg->numPix);
 	
-	//check theres enough space for the message from start, if not, set the start as close to the end as possible
-	if (((img->width*img->height)-start)<(len+1))
-		start = (img->width*img->height) - (len+1);
+		//check theres enough space for the message from start to the end of the image, if not, set the start as close to the end as possible
+		if (((newimg->numPix)-start)<(5*(len+1)))
+			start = (newimg->numPix) - (5*(len+1));
+		//Check the value of the starting red pixel is not equal to the first character of the message, else find a new start red pixel
+	}while (newimg->data[start].red == text[0]);
 	
-	//cycle through string, changing value of red values in array, starting at start and finishing with /0 character
-	for (int i = 0; i<len+1; i++){
-		newimg->data[start+i].red = text[i];
+	//Assign the first char to the start location
+	newimg->data[start].red = text[0];
+	
+	//cycle through string, changing value of (randomly selected) red pixels in newimg->data array, finishing with /0 character.
+	int randomiser;
+	for (int i = 1; i<len+1; i++){
+		do{
+			//select a value between 1 and 5, and check that the red value in the data array is not the same as the character, else randomise again
+			randomiser = ((rand() % 4)+1);
+		}while(newimg->data[start+randomiser].red == text[i]);
+		
+		//change the value of the red pixel at the randomly selected location
+		newimg->data[start+randomiser].red = text[i];
+		//Make sure the next char is placed after the last
+		start += randomiser;
 	}
 	
 	return	newimg;
@@ -133,10 +154,10 @@ char *decode(const struct PPM *oldimg, const struct	PPM	*newimg)
 {
 	/*	TODO: Question 2d */
 	//Create a string for the message, the size cannot be bigger than the image
-	char *message = calloc((newimg->width)*(newimg->height), sizeof(char));
+	char *message = calloc((newimg->numPix), sizeof(char));
 	
 	//Iterate through the arrays until you find a difference
-	for (int i = 0; i < ((newimg->width)*(newimg->height)); i++){
+	for (int i = 0; i < (newimg->numPix); i++){
 		if (oldimg->data[i].red != newimg->data[i].red){
 			//Write the hidden string into message, wait for "/0"
 			int j = 0;
@@ -151,6 +172,14 @@ char *decode(const struct PPM *oldimg, const struct	PPM	*newimg)
 	printf("Error: No difference in files found\n");
 	exit(1);
 }
+
+
+void freePPM(struct PPM *pokemon){
+	
+	free(pokemon->data);
+	free(pokemon);
+}
+
 
 /* TODO: Question 3	*/
 int	main(int argc, char	*argv[])
